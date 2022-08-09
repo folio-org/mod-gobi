7c566bdf9b03302f81f745f0e00bedef269b1096
Dzmitry_Butramyou <dzmitry_butramyou@epam.com>
2022-08-09 17:29:48 +0400

    [MODGOBI-161] - Removed unused import

--- src/main/java/org/folio/gobi/MappingDetailsService.java
+++ src/main/java/org/folio/gobi/MappingDetailsService.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
