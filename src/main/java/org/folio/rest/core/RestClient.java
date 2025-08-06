e9cd5841aba424ae797f89772d91d91ac28673b7
saba_zedginidze <saba_zedginidze@epam.com>
2025-08-06 16:41:37 +0400

    fix(tests): update vertx deployment and closure methods to use onComplete

--- src/main/java/org/folio/rest/core/RestClient.java
+++ src/main/java/org/folio/rest/core/RestClient.java
@@ -19,35 +19,26 @@ import org.folio.rest.tools.utils.TenantTool;
 import io.vertx.core.Context;
 import io.vertx.core.Future;
 import io.vertx.core.MultiMap;
+import io.vertx.core.http.HttpResponseExpectation;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.client.HttpResponse;
 import io.vertx.ext.web.client.WebClient;
-import io.vertx.ext.web.client.predicate.ErrorConverter;
-import io.vertx.ext.web.client.predicate.ResponsePredicate;
 
 public class RestClient {
+
   private static final Logger logger = LogManager.getLogger(RestClient.class);
+  public static final String X_OKAPI_URL = "X-Okapi-Url";
+
   private final WebClient webClient;
-  private final Context ctx;
   private final MultiMap okapiHeaders;
   private final String okapiURL;
   private final String tenantId;
-  public static final String X_OKAPI_URL = "X-Okapi-Url";
-  private static final ErrorConverter ERROR_CONVERTER = ErrorConverter.createFullBody(
-      result -> new HttpException(result.response().statusCode(), result.response().bodyAsString()));
-  private static final ResponsePredicate SUCCESS_RESPONSE_PREDICATE =
-      ResponsePredicate.create(ResponsePredicate.SC_SUCCESS, ERROR_CONVERTER);
 
   public RestClient(Map<String, String> okapiHeaders, Context ctx) {
     okapiURL = okapiHeaders.getOrDefault(X_OKAPI_URL, "");
     tenantId = TenantTool.calculateTenantId(okapiHeaders.get(RestVerticle.OKAPI_HEADER_TENANT));
     webClient = WebClientFactory.getWebClient(ctx.owner());
-    this.ctx = ctx;
     this.okapiHeaders = MultiMap.caseInsensitiveMultiMap().addAll(okapiHeaders);
-  }
-
-  public Context getCtx() {
-    return ctx;
   }
 
   public String getTenantId() {
@@ -57,23 +48,25 @@ public class RestClient {
   public Future<JsonObject> handleGetRequest(String endpoint) {
     logger.debug("Trying to get '{}' object", endpoint);
     return webClient.getAbs(okapiURL + endpoint).putHeaders(okapiHeaders)
-        .expect(SUCCESS_RESPONSE_PREDICATE).send()
-        .map(HttpResponse::bodyAsJsonObject);
+      .send()
+      .compose(RestClient::convertHttpResponse)
+      .map(HttpResponse::bodyAsJsonObject);
   }
 
   /**
    * A common method to update an entry
    *
    * @param recordData json to use for update operation
-   * @param endpoint endpoint
+   * @param endpoint   endpoint
    */
   public Future<Void> handlePutRequest(String endpoint, JsonObject recordData) {
     if (logger.isDebugEnabled()) {
       logger.debug("Trying to put '{}' object with data: {}", endpoint, recordData.encodePrettily());
     }
     return webClient.putAbs(okapiURL + endpoint).putHeaders(okapiHeaders)
-        .expect(SUCCESS_RESPONSE_PREDICATE).sendJsonObject(recordData)
-        .mapEmpty();
+      .sendJsonObject(recordData)
+      .compose(RestClient::convertHttpResponse)
+      .mapEmpty();
   }
 
   public Future<JsonObject> post(String endpoint, JsonObject recordData) {
@@ -81,29 +74,36 @@ public class RestClient {
       logger.debug("Trying to post '{}' object with body: {}", endpoint, recordData.encodePrettily());
     }
     return webClient.postAbs(okapiURL + endpoint).putHeaders(okapiHeaders)
-        .expect(SUCCESS_RESPONSE_PREDICATE).sendJsonObject(recordData)
-        .map(HttpResponse::bodyAsJsonObject);
+      .sendJsonObject(recordData)
+      .compose(RestClient::convertHttpResponse)
+      .map(HttpResponse::bodyAsJsonObject);
   }
 
   public Future<Void> delete(String endpointById) {
     logger.debug("Tying to delete with using endpoint: {}", endpointById);
     return webClient.deleteAbs(okapiURL + endpointById)
-        .putHeaders(okapiHeaders).putHeader("Accept", APPLICATION_JSON + ", " + TEXT_PLAIN)
-        .expect(SUCCESS_RESPONSE_PREDICATE).send()
-        .mapEmpty();
+      .putHeaders(okapiHeaders).putHeader("Accept", APPLICATION_JSON + ", " + TEXT_PLAIN)
+      .send()
+      .compose(RestClient::convertHttpResponse)
+      .mapEmpty();
   }
 
   public Future<JsonObject> handleGetRequest(String baseEndpoint, String query, int offset, int limit) {
     String endpoint = String.format(SEARCH_ENDPOINT, baseEndpoint, limit, offset, buildQueryParam(query));
-
     return handleGetRequest(endpoint);
   }
 
-  public String buildQueryParam(String query) {
+  private static <T> Future<HttpResponse<T>> convertHttpResponse(HttpResponse<T> response) {
+    return HttpResponseExpectation.SC_SUCCESS.test(response)
+      ? Future.succeededFuture(response)
+      : Future.failedFuture(new HttpException(response.statusCode(), response.bodyAsString()));
+  }
+
+  private static String buildQueryParam(String query) {
     return isEmpty(query) ? EMPTY : "&query=" + encodeQuery(query);
   }
 
-  public String encodeQuery(String query) {
+  private static String encodeQuery(String query) {
     return URLEncoder.encode(query, StandardCharsets.UTF_8);
   }
 
