bb7a46b6a875df3177219bf00d170536a2271e9f
Serhii_Nosko <Serhii_Nosko@epam.com>
2025-11-24 14:20:53 +0300

    MODGOBI-216. Replace mod-configuration for retrieving configuration address

--- src/test/java/org/folio/gobi/LookupServiceTest.java
+++ src/test/java/org/folio/gobi/LookupServiceTest.java
@@ -156,7 +156,7 @@ public class LookupServiceTest {
     doReturn(succeededFuture(new JsonObject(address)))
       .when(restClient).handleGetRequest(argThat(endpoint -> endpoint.startsWith("/settings/entries")));
 
-    var jsonAddress = lookupService.lookupConfigAddress("address").join();
+    var jsonAddress = lookupService.lookupConfigAddress("Test Address").join();
 
     assertNotNull(jsonAddress);
   }
